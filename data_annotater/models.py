d3cacda9560f17a0402924849cf696388ffb572c
Michael McGraw <michaelmcgraw09@gmail.com>
2026-01-31 22:03:17 -0500

    model setup

--- data_annotater/models.py
+++ data_annotater/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class RetailRow(models.Model):
+    class Segment(models.TextChoices):
+        FIRST_PARTY = "FIRST_PARTY", "First Party"
+        THIRD_PARTY = "THIRD_PARTY", "Third Party"
+
+    merchant = models.CharField(max_length=100)
+    sku = models.CharField(max_length=100)
+    country = models.CharField(max_length=2)
+
+    retailer = models.CharField(max_length=100, blank=True)
+    segment = models.CharField(
+        max_length=32,
+        choices=Segment.choices,
+        null=True,
+        blank=True,
+    )
+    
+    class Meta:
+            constraints = [
+                models.UniqueConstraint(
+                    fields=["merchant", "sku", "country"],
+                    name="uniq_merchant_sku_country",
+                )
+            ]
