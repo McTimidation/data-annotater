c18876296257a9d75c7560ebfa86d51e2df73e87
Michael McGraw <michaelmcgraw09@gmail.com>
2026-02-04 11:10:39 -0500

    corrected pathing

--- data_annotater/urls.py
+++ data_annotater/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "data_annotater"
 
 urlpatterns = [
-    path("upload/", views.upload, name="upload_csv"),
+    path("upload/", views.upload, name="upload"),
     path("list/", views.retail_list, name="list"),
     path("edit/<int:pk>/", views.edit, name="edit"),
 ]
